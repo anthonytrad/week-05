59f89bc6ed19cfe4b8796742c96026116f0b0aa9
Anthony Trad <anthonytrad1984@gmail.com>
2016-03-30 13:33:42 -0700

    Week 05

---
+++ lesson-2/js/scripts.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $("button#show-hide").click(function(){
+        $("h2#how-to, img").toggle();
+    });
+});
---
+++ lesson-7/js/scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+
+	    var target = this.hash;
+	    var $target = $(target);
+
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 900, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+});
+
+// Flex Slider
+$(window).load(function() {
+  $('#carousel').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    itemWidth: 210,
+    itemMargin: 5,
+    asNavFor: '#slider'
+  });
+ 
+  $('#slider').flexslider({
+    animation: "slide",
+    controlNav: false,
+    animationLoop: false,
+    slideshow: false,
+    sync: "#carousel"
+  });
+});
